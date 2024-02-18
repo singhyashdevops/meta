23b44ba09239b7c9bb1bac77a79a1bf5fa8999b4
singhyashdevops <singhyashdevops@gmail.com>
2024-02-18 11:42:29 +0530

    6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/alt-text */
 // import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
+import Navbar from './components/Navbar'
+import Footer from "./components/Footer"
+import TextUtils from './components/TextUtils'
+import MovingText from "./components/MovingText"
+import CardMenu from './components/CardMenu'
+import Contact from './components/Contact'
+import LogForm from './components/LogForm'
+import Tags from './components/Tags'
+import Companies from './components/Companies'
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +36,27 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//       <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/android/android-plain.svg" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
